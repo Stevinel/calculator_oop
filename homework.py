3cee414eeececccd9c52b8e1b623c8e4e682e783
Станислав <stevinel@xaker.ru>
2020-10-12 15:01:49 +0300

    fixes

--- homework.py
+++ homework.py
@@ -1,5 +1,6 @@
 import datetime as dt
-date_format = '%d.%m.%Y'
+DATE_FORMAT = '%d.%m.%Y'
+
 
 class Calculator:
     def __init__(self, limit):
@@ -11,57 +12,66 @@ class Calculator:
 
     def get_today_stats(self):
         today_stats_count = 0
-        today_date = dt.datetime.now().date()
-        for i in self.records:
-            if i.date == today_date:
-                today_stats_count += i.amount
+        today_date = dt.date.today()
+        for record in self.records:
+            if record.date == today_date:
+                today_stats_count += record.amount
         return today_stats_count
-    
+
     def get_week_stats(self):
         week_stats = 0
-        today = dt.datetime.now().date()
+        today = dt.date.today()
         date_week_ago = (today - dt.timedelta(days=7))
-        for i in self.records:
-            if date_week_ago <= i.date <= today:
-                week_stats += i.amount
+        for record in self.records:
+            if date_week_ago <= record.date <= today:
+                week_stats += record.amount
         return week_stats
 
+    def get_remained(self):
+        return self.limit - self.get_today_stats()
+  
+
 class CashCalculator(Calculator):
-    USD_RATE = 77.48
-    EURO_RATE = 91.06
+    USD_RATE = 77.12
+    EURO_RATE = 90.95
 
     def get_today_cash_remained(self, currency):
-        remains = self.limit - self.get_today_stats()
-      
-        if currency == 'usd':
-            cur = (f'{round(abs(remains/self.USD_RATE), 2)} USD')
-        elif currency == 'eur':
-            cur = (f'{round(abs(remains/self.EURO_RATE), 2)} Euro')
-        else:
-            cur = (f'{abs(remains)} руб')
+        CUR_LIB = {
+            'rub': (1, 'руб'),
+            'usd': (self.USD_RATE, 'USD'),
+            'eur': (self.EURO_RATE, 'Euro')
+        }
 
-        if remains > 0:
-            return (f'На сегодня осталось {cur}')
-        elif remains < 0:
-            return (f'Денег нет, держись: твой долг - {cur}')
-        else:
+        if currency not in CUR_LIB.keys():
+            return 'Валюта не поддерживается'
+
+        if self.get_remained() == 0:
             return 'Денег нет, держись'
-            
+
+        cur = CUR_LIB[currency]
+        remains = round(abs(self.get_remained()) / cur[0], 2)
+        if self.get_remained() > 0:
+            return (f'На сегодня осталось {remains} {cur[1]}')
+        else:
+            return (f'Денег нет, держись: твой долг - {remains} {cur[1]}')
+
+
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        remains = self.limit - self.get_today_stats()
-        print(self.get_today_stats())
-        if remains > 0:
-            return (f'Сегодня можно съесть что-нибудь ещё,\
- но с общей калорийностью не более {remains} кКал')
+        if self.get_remained() > 0:
+            return (
+                'Сегодня можно съесть что-нибудь ещё, '
+                f'но с общей калорийностью не более {self.get_remained()} кКал'
+            )
         else:
             return 'Хватит есть!'
-    
+
+
 class Record():
-    def __init__(self, amount, comment, date = None):
+    def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
         if date is None:
-            self.date = dt.datetime.now().date()
+            self.date =  dt.date.today()
         else:
-            self.date = dt.datetime.strptime(date, date_format).date()
+            self.date = dt.datetime.strptime(date, DATE_FORMAT).date()
